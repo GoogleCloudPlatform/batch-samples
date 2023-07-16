69055b99a6c28d0b0026737465caae818edca656
bolianyin@gmail.com <bolianyin@gmail.com>
2023-07-16 18:36:31 +0000

    Minor comment update

--- api-samples/env.sh
+++ api-samples/env.sh
@@ -1,4 +1,7 @@
-# Google Cloud project
+# UPDATE TO MATCH YOUR SETTINGS.
+# Common variables for all samples.
+
+# Google Cloud project ID.
 ProjectID=batch-api-samples
 
 # The region where the Batch job is submitted.
--- api-samples/pubsub/submit.sh
+++ api-samples/pubsub/submit.sh
@@ -6,7 +6,9 @@ set -o allexport
 # Common variables.
 source ../env.sh
 
+# UPDATE TO MATCH YOUR SETTINGS.
 # Variables for this sample.
+# -----------------------------------------
 # Pub/Sub topic for job state notification.
 JobStateTopic=batch-job-state
 # Pub/Sub topic for task state notification.
