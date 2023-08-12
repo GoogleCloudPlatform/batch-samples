811a77cb3b32f073e3894bcfceae7cff43bbed41
WenLiGCP <wnli@google.com>
2023-08-11 18:23:10 -0700

    add a job with localssd volume sample

---
+++ api-samples/localssd/README.md
@@ -0,0 +1,13 @@
+## Overview
+Google Batch supports jobs using local SSDs as the storage volume. Each local SSD
+is 375 GB, so the size of the local SSDs required in total must be a multiple of
+375 GB. For example, for 2 local SSDs, set this value to 750 GB.
+
+## Prerequisites
+* Check your project has [enough quota](https://cloud.google.com/compute/resource-usage#disk_quota) for local ssds.
+* Fill in the values for variables for the samples in `submit.sh`.
+
+## LocalSSD Samples
+### job_with_localssd_volume.json
+This script job echoes `hello world` to ${MOUNT_PATH}/output_task_${BATCH_TASK_INDEX}.txt
+file for each task.
---
+++ api-samples/localssd/submit.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Export all variables.
+set -o allexport
+
+# Common variables.
+source ../env.sh
+
+# UPDATE TO MATCH YOUR SETTINGS.
+# Variables for this sample.
+# -----------------------------------------
+# DEVICE_NAME is the device name of local ssds.
+DEVICE_NAME=mylocalssd
+# MACHINE_TYPE is the machine type you want to use for this job.
+# The allowed number of local SSDs depends on the machine type.
+MACHINE_TYPE=n1-standard-1
+# Mount path of the volume.
+MOUNT_PATH=/mnt/disks/localssd
+# LOCAL_SSD_SIZE is the size of local ssd volume required. It should
+# be a multiple of 375 GB.
+LOCAL_SSD_SIZE=750
+
+# Turn OFF the allexport option.
+set +o allexport
+
+gcloud batch jobs submit --job-prefix=localssd --location=${Location} --project=${ProjectID} --config - <<EOF
+$(envsubst '$DEVICE_NAME','$MACHINE_TYPE','$MOUNT_PATH','$LOCAL_SSD_SIZE' < ./job_with_localssd_volume.json)
+EOF
